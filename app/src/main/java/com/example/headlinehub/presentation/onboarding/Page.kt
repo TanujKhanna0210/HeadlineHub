9a37e9c2194d0d4897aa45201e2ad082644a2e19
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-31 21:40:32 +0530

    Creating onbaording Page data class

---
+++ app/src/main/java/com/example/headlinehub/presentation/onboarding/Page.kt
@@ -0,0 +1,28 @@
+package com.example.headlinehub.presentation.onboarding
+
+import androidx.annotation.DrawableRes
+import com.example.headlinehub.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int
+)
+
+val pages = listOf(
+    Page(
+        title = "Welcome to HeadlineHub!",
+        description = "Your go-to source for breaking news and personalized updates.\nStay informed with the latest stories, tailored just for you.",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "Explore Diverse Perspectives",
+        description = "Immerse yourself in a variety of topics, from global affairs to niche interests.",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "Save and Access Anytime",
+        description = "Found an article you love? Save it for later!\nTap on the bookmark icon on the top of your screen to save an article for later.",
+        image = R.drawable.onboarding3
+    )
+)
