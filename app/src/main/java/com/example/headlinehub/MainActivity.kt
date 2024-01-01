83c4b7361852d950b6469f6c44f02689b8ae97b1
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-01-02 01:42:22 +0530

    Creating main view model

--- app/src/main/java/com/example/headlinehub/MainActivity.kt
+++ app/src/main/java/com/example/headlinehub/MainActivity.kt
@@ -1,44 +1,35 @@
 package com.example.headlinehub
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.example.headlinehub.domain.usecases.AppEntryUseCases
-import com.example.headlinehub.presentation.onboarding.OnBoardingScreen
-import com.example.headlinehub.presentation.onboarding.OnBoardingViewModel
+import com.example.headlinehub.presentation.navgraph.NavGraph
 import com.example.headlinehub.ui.theme.HeadlineHubTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            useCases.readAppEntry().collect {
-                Log.d("Test", it.toString())
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
             }
         }
-
         setContent {
             HeadlineHubTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
