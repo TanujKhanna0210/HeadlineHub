4c10666a51cfdf72c39d3d09066ba9ddab4b354a
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-01-02 20:10:14 +0530

    Creating article card composable

--- app/src/main/java/com/example/headlinehub/presentation/Dimens.kt
+++ app/src/main/java/com/example/headlinehub/presentation/Dimens.kt
@@ -11,4 +11,10 @@ object Dimens {
 
     val PageIndicatorWidth = 52.dp
 
+    val ArticleCardSize = 96.dp
+
+    val ExtraSmallPadding = 3.dp
+    val ExtraSmallPadding2 = 6.dp
+
+    val SmallIconSize = 11.dp
 }
