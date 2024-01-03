c12ff7b3e7b9668721f2829637ec6ca0c6e3f6a8
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-01-04 03:39:02 +0530

    Injecting room dependencies and creating database

--- app/src/main/java/com/example/headlinehub/di/AppModule.kt
+++ app/src/main/java/com/example/headlinehub/di/AppModule.kt
@@ -1,6 +1,10 @@
 package com.example.headlinehub.di
 
 import android.app.Application
+import androidx.room.Room
+import com.example.headlinehub.data.local.NewsDao
+import com.example.headlinehub.data.local.NewsDatabase
+import com.example.headlinehub.data.local.NewsTypeConverter
 import com.example.headlinehub.data.manager.LocalUserManagerImpl
 import com.example.headlinehub.data.remote.NewsApi
 import com.example.headlinehub.data.repository.NewsRepositoryImpl
@@ -13,6 +17,7 @@ import com.example.headlinehub.domain.usecases.news.GetNews
 import com.example.headlinehub.domain.usecases.news.NewsUseCases
 import com.example.headlinehub.domain.usecases.news.SearchNews
 import com.example.headlinehub.util.Constants.BASE_URL
+import com.example.headlinehub.util.Constants.NEWS_DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -66,5 +71,25 @@ object AppModule {
             searchNews = SearchNews(newsRepository)
         )
     }
+
+    @Provides
+    @Singleton
+    fun providesNewsDatabase(
+        application: Application
+    ): NewsDatabase {
+        return Room.databaseBuilder(
+            context = application,
+            klass = NewsDatabase::class.java,
+            name = NEWS_DATABASE_NAME
+        ).addTypeConverter(NewsTypeConverter())
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNewsDao(
+        newsDatabase: NewsDatabase
+    ): NewsDao = newsDatabase.newsDao
 
 }
