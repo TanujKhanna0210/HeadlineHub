e32ddeb2224c2cf1290dae850c15840e81000732
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-01-05 22:08:24 +0530

    Updating the onboarding images

--- app/src/main/java/com/example/headlinehub/presentation/onboarding/Page.kt
+++ app/src/main/java/com/example/headlinehub/presentation/onboarding/Page.kt
@@ -13,16 +13,16 @@ val pages = listOf(
     Page(
         title = "Welcome to HeadlineHub!",
         description = "Your go-to source for breaking news and personalized updates.\nStay informed with the latest stories, tailored just for you.",
-        image = R.drawable.onboarding1
+        image = R.drawable.onboarding_1
     ),
     Page(
         title = "Explore Diverse Perspectives",
         description = "Immerse yourself in a variety of topics, from global affairs to niche interests.",
-        image = R.drawable.onboarding2
+        image = R.drawable.onboarding_2
     ),
     Page(
         title = "Save and Access Anytime",
         description = "Found an article you love? Save it for later!",
-        image = R.drawable.onboarding3
+        image = R.drawable.onboarding_3
     )
 )
